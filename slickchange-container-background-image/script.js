c7d73a320bef255b48f4a10ed7c57ade8be36970
vijaychauhanssn <vijaychauhanssn@gmail.com>
2021-05-09 11:23:58 +0530

    slick slide

---
+++ slickchange-container-background-image/script.js
@@ -0,0 +1,29 @@
+
+const setBgImage = (ele) => {
+  if (ele.data("background")) {
+    ele.closest(".slider-container").css("background", ele.data("background"));
+  }
+};
+$(".slider1")
+  .on("init", (slick) => {
+    setBgImage($(slick.target).find(".slick-active"));
+  })
+  .slick()
+  .on("beforeChange", (event, slick, currentSlide, nextSlide) => {
+    setBgImage(slick.$slider.find(`[data-slick-index=${nextSlide}]`));
+  });
+  
+  const setColor = (ele) => {
+  if (ele.data("color")) {
+    ele.closest(".slider-container").css("background-color", ele.data("color"));
+  }
+};
+
+$(".slider2")
+  .on("init", (slick) => {
+    setColor($(slick.target).find(".slick-active"));
+  })
+  .slick()
+  .on("beforeChange", (event, slick, currentSlide, nextSlide) => {
+    setColor(slick.$slider.find(`[data-slick-index=${nextSlide}]`));
+  });
